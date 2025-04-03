0981b3ccf1f6a74460b79b93bf7d0234831a0eae
yu-yake2002 <yuyake02@outlook.com>
2025-04-03 16:32:44 +0800

    feat(core): to load/store in AME
    - Implement matrix load and store controller in Backend and MemBlock
    - Update related Bundle to support AME
    - Adjust TLB and LSQ, but there are still some bugs in LSQ now

--- src/main/scala/xiangshan/mem/MemCommon.scala
+++ src/main/scala/xiangshan/mem/MemCommon.scala
@@ -192,6 +192,12 @@ class LsPipelineBundle(implicit p: Parameters) extends XSBundle
   val updateAddrValid     = Bool()
 }
 
+class MlsPipelineBundle(implicit p: Parameters) extends LsPipelineBundle {
+  val stride = UInt(PAddrBits.W)
+  val mtile0 = UInt(XLEN.W)
+  val mtile1 = UInt(XLEN.W)
+}
+
 class LdPrefetchTrainBundle(implicit p: Parameters) extends LsPipelineBundle {
   val meta_prefetch = UInt(L1PfSourceBits.W)
   val meta_access = Bool()
