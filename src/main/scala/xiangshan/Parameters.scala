45c74cded34724feb50662453292407778ae9fb4
yu-yake2002 <yuyake02@outlook.com>
2025-04-18 15:27:08 +0800

    fix(matrix): Use real MLEN and RLEN in AME

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -63,9 +63,9 @@ case class XSCoreParameters
   VLEN: Int = 128,
   ELEN: Int = 64,
   HSXLEN: Int = 64,
-  MLEN: Int = 64,
-  RLEN: Int = 64,
-  AMUL: Int = 4,
+  MLEN: Int = 0x20000, // 64 * 256 * 8
+  RLEN: Int = 0x00800, // 256 * 8
+  AMUL: Int = 4,       // 32 / 8
   HasMExtension: Boolean = true,
   HasCExtension: Boolean = true,
   HasHExtension: Boolean = true,
